dc26b2502543c25c6d6dd273e5c8dacde4de7dfd
sungpark Kim <urstory@gmail.com>
2015-08-19 16:58:11 +0900

    study

---
+++ day02/src/Test08.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class Test08 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("문장열을 입력하세요.");
+        String line = scan.next(); // 키보드로 입력한다.
+        System.out.println(line);
+    }
+}
