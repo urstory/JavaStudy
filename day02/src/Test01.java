8f527287698c683e11e471219026b9abafad5d8a
sungpark Kim <urstory@gmail.com>
2015-08-19 13:18:30 +0900

    study

---
+++ day02/src/Test01.java
@@ -0,0 +1,29 @@
+public class Test01 {
+    public static void main(String[] args){
+        int sum = 0;
+        // 1) int i = 1
+        // 2) i <= 100 만족하면 블럭을 실행
+        // 3) i++
+        // 4) 2번부터 다시 시작.
+        for(int i = 1; i <= 100; i++){
+            sum = sum + i;
+        }
+        System.out.println(sum);
+
+        sum = 0;
+        int k = 1;
+        while(k <= 100){
+            sum = sum + k;
+            k++;
+        }
+        System.out.println(sum);
+
+        sum = 0;
+        int m = 1;
+        do{
+            sum = sum + m;
+            m++;
+        }while(m <= 100);
+        System.out.println(sum);
+    }
+}
